9c16040fc94a1413918d906234a2551263bc9935
Justin Kusz <justin.kusz@gmail.com>
2018-12-10 16:23:14 -0500

    Initial commit

---
+++ server/models/recommendation.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const isUrl = require('validator/lib/isURL');
+
+const Recommendation = mongoose.model('Recommendation', {
+    type: {
+        type: String,
+        required: true,
+        trim: true,
+        enum: ['album', 'book', 'show', 'movie']
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    url: {
+        type: String,
+        trim: true,
+        validate: {
+            validator: isUrl
+        }
+    },
+    recommender: {
+        type: String,
+        trim: true,
+        required: true
+    }
+});
+
+module.exports = { Recommendation };
---
+++ server/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { mongoose } = require('./db/mongoose');
+
+const { Recommendation } = require('./models/recommendation');
+
+const port = process.env.NODE_SERVER_PORT || 4000;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/recs', (req, res) => {
+    const { type, title, url, recommender } = req.body;
+
+    const newRec = new Recommendation({ type, title, url, recommender });
+
+    newRec.save().then((rec) => {
+        res.status(200).send(rec);
+    }, (error) => {
+        res.status(400).send(error);
+    });
+});
+
+app.get('/recs', (req, res) => {
+    Recommendation.find().then((recs) => {
+        res.status(200).send({recs: recs});
+    }, (error) => {
+        res.status(400).send(error);
+    });
+});
+
+app.get('/recs/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send('Invalid id');
+    } else {
+        Recommendation.findById(id).then((rec) => {
+            if (!rec) {
+                return res.status(404).send();
+            }
+            res.status(200).send({rec: rec});
+        }).catch((error) => {
+            res.status(400).send(error)
+        });
+    }
+});
+
+if (process.env.NODE_ENV !== 'testing') {
+    app.listen(port, () => {
+        console.log(`Server started at http://localhost:${port}`);
+    });
+}
+
+module.exports.app = app;
