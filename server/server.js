0292b1c503298c49ae0cd0c8a882a4bdd8cbcf84
Justin Kusz <justin.kusz@gmail.com>
2018-12-23 17:58:48 -0500

    Send empty object to mitigate XML Parsing Error in Firefox

--- server/server.js
+++ server/server.js
@@ -101,7 +101,7 @@ app.get('/users/me', authenticate, (req, res) => {
 
 app.delete('/users/me/token', authenticate, (req, res) => {
     req.user.removeToken(req.token).then(() => {
-        res.status(200).send();
+        res.status(200).send({});
     }).catch((err) => {
         res.status(400).send(err.message);
     });
